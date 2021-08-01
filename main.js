4efcc1ce5a008f5685aafaef54861cefd912a200
shreenesh <83068316+shreenesh@users.noreply.github.com>
2021-08-01 13:40:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,3 +8,25 @@ Webcam.set({
 
 camera=document.getElementById("camera");
 Webcam.attach('#camera');
+ 
+function take_snap(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src ="'+data_uri+'"/>';
+    });
+}
+console.log("ml5 version",ml5.version);
+classifier=ml5.imageClassifier("MobileNet",modelloaded);
+function modelloaded() {
+    console.log("modelloaded");
+}
+function check(){
+    img=document.getElementById("captured_image");
+    classifier.classify(img,gotresult);
+}
+function gotresult(error,results) {
+    if(error){
+        console.error(error);
+    }
+    else{console.log(result);
+    document.getElementById("object_name").innerHTML=results[0].label;}
+}
